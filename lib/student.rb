6f94736625636ebc319618c7375a467e2cf21b17
Rebecca Tong <rtong@buffalo.edu>
2018-04-18 18:42:27 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,7 @@ class Student
     FROM students
     SQL
     students = DB[:conn].execute(sql)
-    self.new_from_db(sql)
+    self.new_from_db(students)
     binding.pry
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
