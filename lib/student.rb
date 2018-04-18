463e786c4d5ed8d59437476621c2929a7ddd2a1d
Rebecca Tong <rtong@buffalo.edu>
2018-04-18 18:49:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,6 +65,13 @@ class Student
   end
 
   def count_all_students_in_grade_9
+    sql = <<-SQL
+    SELECT * 
+    FROM students
+    WHERE grade = 9
+    SQL 
+    
+  DB[:conn].execute(sql)
   end
 
   def students_below_12th_grade
