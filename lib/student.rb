eb420b5cc9a2c128e0be7541a66a2fb2534603e0
Rebecca Tong <rtong@buffalo.edu>
2018-04-18 19:13:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -74,6 +74,12 @@ class Student
   end
 
   def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade < 12
+    SQL
+    DB[:conn].execute(sql)
   end
 
   def self.first_X_students_in_grade_10
