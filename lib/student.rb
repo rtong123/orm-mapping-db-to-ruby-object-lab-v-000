5a64d0a3646a74683587457a6aa6c278d1680af5
Rebecca Tong <rtong@buffalo.edu>
2018-04-18 19:17:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -89,7 +89,9 @@ class Student
     WHERE grade = ?
     LIMIT 1
     SQL
-    DB[:conn].execute(sql,grade)
+    DB[:conn].execute(sql,grade).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def self.first_student_in_grade_10
