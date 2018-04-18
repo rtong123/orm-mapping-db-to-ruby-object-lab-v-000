2d251914e7af038e6748a12972a69abaf3d98927
Rebecca Tong <rtong@buffalo.edu>
2018-04-18 18:30:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,7 +31,7 @@ class Student
     WHERE name = ?
     LIMIT 1
     SQL
-    DB[:conn].excecute(sql,name).map do |row|
+    DB[:conn].execute(sql,name).map do |row|
       self.new_from_db(row)
     end.first
   end
