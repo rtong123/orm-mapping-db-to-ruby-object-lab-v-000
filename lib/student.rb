4f8e65062e284c666a5fbb626c8efb5530d525aa
Rebecca Tong <rtong@buffalo.edu>
2018-04-18 19:55:56 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -99,13 +99,13 @@ class Student
     SELECT *
     FROM students
     WHERE grade = 10
-    ORDER BY students.id 
+    ORDER BY students.id
     LIMIT 1
     SQL
 
     DB[:conn].execute(sql).map do |row|
     self.new_from_db(row)
-    end
+    end.first
   end
 
   def self.all_students_in_grade_X(grade)
