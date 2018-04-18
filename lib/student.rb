281cf82dd551d915cd3cdebd3a588202a2c56596
Rebecca Tong <rtong@buffalo.edu>
2018-04-18 19:21:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -106,11 +106,11 @@ class Student
   end
 
   def self.all_students_in_grade_X(grade)
-    sql = <<-SQL
-    SELECT *
-    FROM students
-    WHERE grade = ?
-    SQL
-
-    DB[:conn].execute(sql,grade)
+    # sql = <<-SQL
+    # SELECT *
+    # FROM students
+    # WHERE grade = ?
+    # SQL
+    #
+    # DB[:conn].execute(sql,grade)
 end
